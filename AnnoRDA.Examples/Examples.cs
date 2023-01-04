a858340bd7270acbdf6283c62be05a7b94f670a1
taubenangriff <51975164+taubenangriff@users.noreply.github.com>
2023-01-04 20:36:17 +0100

    add examples project

---
+++ AnnoRDA.Examples/Examples.cs
@@ -0,0 +1,14 @@
+﻿using AnnoRDA.Loader;
+using System.Diagnostics;
+
+FileSystemLoader loader = new FileSystemLoader("F:\\SteamLibrary\\steamapps\\common\\Anno 1800\\maindata");
+Console.WriteLine(String.Join("\n", loader.Archives));
+
+Stopwatch stopwatch = Stopwatch.StartNew();
+var filesystem = loader.Load();
+stopwatch.Stop();
+Console.WriteLine($"loaded RDA System in " + stopwatch.Elapsed.TotalMilliseconds + " ms");
+
+using var templates = filesystem.OpenRead("data/config/export/main/asset/templates.xml");
+using var filestream = File.Create("templates.xml");
+templates.CopyTo(filestream);

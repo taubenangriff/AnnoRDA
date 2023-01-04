c6afddd623c2c3bc7b01066a3fc42f16e83ce294
taubenangriff <51975164+taubenangriff@users.noreply.github.com>
2023-01-04 11:38:33 +0100

    add DataArchive Project

---
+++ AnnoRDA.DataArchives/DataArchiveBuilder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnnoRDA.DataArchives
+{
+    public class DataArchiveBuilder
+    {
+        private DataArchiveBuilder() { }
+
+        public static DataArchiveBuilder New()
+        { 
+            return new DataArchiveBuilder();
+        }
+
+        public DataArchive Build()
+        {
+            return new DataArchive();
+        }
+    }
+}
---
+++ AnnoRDA.DataArchives/IDataArchive.cs
@@ -0,0 +1,17 @@
+﻿namespace AnnoRDA.DataArchives
+{
+    internal interface IDataArchive
+    {
+        bool IsValid { get; }
+        string Path { get; }
+
+        IEnumerable<string> Files { get; }
+        IEnumerable<string> FilesFor(params string[] extensions);
+
+        Stream? OpenRead(string filePath);
+        IEnumerable<string> Find(string pattern);
+
+        //this should go vanish itself into the builder
+        Task LoadAsync(params string[] forEndings);
+    }
+}

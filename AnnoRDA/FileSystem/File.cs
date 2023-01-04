eeb19c43a1a6da50ddb9887e3798c9840edd2797
taubenangriff <51975164+taubenangriff@users.noreply.github.com>
2023-01-04 20:35:32 +0100

    DebuggerDisplay for file

--- AnnoRDA/FileSystem/File.cs
+++ AnnoRDA/FileSystem/File.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AnnoRDA
 {
-
+    [DebuggerDisplay("File: {Name}")]
     public class File : IFileSystemItem
     {
         public string Name { get; set; }
